c85310fefe3ea87955cc1c4e4ffd234ed773fc11
swiftcc624 <swiftcc624@gmail.com>
2023-08-31 16:03:45 +0800

    first version

---
+++ index.js
@@ -0,0 +1,42 @@
+const http = require("http");
+const fs = require("fs");
+const path = require("path");
+
+const server = http.createServer((req, res) => {
+  // Determine the file extension of the requested file
+  const ext = path.extname(req.url);
+
+  // Serve HTML files as text/html
+  if (ext === ".html") {
+    fs.readFile("/home/rover/projects/z/mapbox/" + req.url, (err, data) => {
+      if (err) {
+        res.writeHead(404, { "Content-Type": "text/plain" });
+        res.end("File not found");
+      } else {
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.end(data);
+      }
+    });
+  }
+  // Serve JavaScript files as text/javascript
+  else if (ext === ".geojson" || ext === ".json") {
+    fs.readFile("/home/rover/projects/z/mapbox/" + req.url, (err, data) => {
+      if (err) {
+        res.writeHead(404, { "Content-Type": "text/plain" });
+        res.end("File not found");
+      } else {
+        res.writeHead(200, { "Content-Type": "text/content" });
+        res.end(data);
+      }
+    });
+  }
+  // Return a 404 error for other file types
+  else {
+    res.writeHead(404, { "Content-Type": "text/plain" });
+    res.end("File not found");
+  }
+});
+
+server.listen(3000, () => {
+  console.log("Server running at http://localhost:3000/");
+});
---
+++ merge.js
@@ -0,0 +1,36 @@
+// merge gadm41_CAN_1.json and gadm41_USA_1.json into gadm41_MERGED_1.json
+
+const fs = require("fs");
+
+const merge = () => {
+  const canada = JSON.parse(fs.readFileSync("./gadm41_CAN_1.json"));
+  const usa = JSON.parse(fs.readFileSync("./gadm41_USA_1.json"));
+  const mexico = JSON.parse(fs.readFileSync("./gadm41_MEX_1.json"));
+  const merged = canada.features.concat(usa.features).concat(mexico.features);
+
+  canada.features = merged.map((feature, index) => {
+    return {
+      ...feature,
+      id: index + 1,
+      // id: feature.properties.GID_1,
+    };
+  });
+
+  // let tolerance = 0.005; // Adjust this value as per your needs
+  // let highQuality = false; // Set to true for a slower, but more accurate simplification
+
+  // let simplifiedData = turf.simplify(canada, {
+  //   tolerance: tolerance,
+  //   highQuality: highQuality,
+  // });
+
+  fs.writeFileSync("./gadm41_MERGED_1.json", JSON.stringify(canada));
+};
+
+merge();
+
+// 10084  npm install -g mapshaper\n
+// 10085  mapshaper -i gadm41_MERGED_1.json -simplify dp 10% -o output.geojson\n
+// 10086  du -msh output.geojson\n
+// 10087  ls
+// 10088  mapshaper -i gadm41_MERGED_1.json -simplify dp 1% -o output.geojson\n
