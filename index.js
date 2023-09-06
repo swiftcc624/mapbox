a513e905cf729c4a9e919c402b0dd42e35921802
swiftcc624 <swiftcc624@gmail.com>
2023-09-06 16:31:29 +0800

    fix base dir path

--- index.js
+++ index.js
@@ -2,13 +2,18 @@ const http = require("http");
 const fs = require("fs");
 const path = require("path");
 
+// const baseDir = "/home/rover/projects/z/mapbox/";
+
+const baseDir = path.join(__dirname, "/");
+console.log(baseDir);
+
 const server = http.createServer((req, res) => {
   // Determine the file extension of the requested file
   const ext = path.extname(req.url);
 
   // Serve HTML files as text/html
   if (ext === ".html") {
-    fs.readFile("/home/rover/projects/z/mapbox/" + req.url, (err, data) => {
+    fs.readFile(baseDir + req.url, (err, data) => {
       if (err) {
         res.writeHead(404, { "Content-Type": "text/plain" });
         res.end("File not found");
@@ -20,7 +25,7 @@ const server = http.createServer((req, res) => {
   }
   // Serve JavaScript files as text/javascript
   else if (ext === ".geojson" || ext === ".json") {
-    fs.readFile("/home/rover/projects/z/mapbox/" + req.url, (err, data) => {
+    fs.readFile(baseDir + req.url, (err, data) => {
       if (err) {
         res.writeHead(404, { "Content-Type": "text/plain" });
         res.end("File not found");
