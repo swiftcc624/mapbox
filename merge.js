33699686d27a5e17622105759a99de217e145522
swiftcc624 <swiftcc624@gmail.com>
2023-09-08 01:39:16 +0800

    update mapbox style, show only USA,CA,Mexico and don't show states for Mexico

--- merge.js
+++ merge.js
@@ -5,7 +5,7 @@ const fs = require("fs");
 const merge = () => {
   const canada = JSON.parse(fs.readFileSync("./gadm41_CAN_1.json"));
   const usa = JSON.parse(fs.readFileSync("./gadm41_USA_1.json"));
-  const mexico = JSON.parse(fs.readFileSync("./gadm41_MEX_1.json"));
+  const mexico = JSON.parse(fs.readFileSync("./gadm41_MEX_0_output1.geojson"));
   const merged = canada.features.concat(usa.features).concat(mexico.features);
 
   canada.features = merged.map((feature, index) => {
@@ -34,3 +34,7 @@ merge();
 // 10086  du -msh output.geojson\n
 // 10087  ls
 // 10088  mapshaper -i gadm41_MERGED_1.json -simplify dp 1% -o output.geojson\n
+
+
+// mapshaper -i gadm41_MEX_0.json -simplify dp 1% -o gadm41_MEX_0_output.geojson
+// mapshaper -i gadm41_MEX_0_output.geojson -simplify dp 10% -o gadm41_MEX_0_output1.geojson
